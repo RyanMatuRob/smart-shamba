12ec82dd7f5f15c023efdfdb6608a951696ba772
skthagr8 <samuel.kahiga@strathmore.edu>
2026-07-03 09:21:44 +0300

    Minor Rectifications

--- sql-database/main.py
+++ sql-database/main.py
@@ -21,11 +21,20 @@ CLIENT_ID = "TTGO"
 
 # Sensor Pins
 DHT_PIN = 23
-SOIL_PIN = 15       # Change if your board uses another ADC pin
+SOIL_PIN = 15      
 
 # Soil Calibration
 DRY_VALUE = 4095
 WET_VALUE = 1200
+
+# Sensor Pins
+DHT_PIN = 23
+SOIL_PIN = 15
+PH_PIN = 32
+
+# pH Calibration
+PH_SLOPE = -5.70
+PH_OFFSET = 21.34
 
 # ============================================================
 # STARTUP
@@ -76,6 +85,7 @@ except Exception as e:
     print(e)
     raise SystemExit
 
+
 # ============================================================
 # TEST SOIL SENSOR
 # ============================================================
@@ -91,6 +101,30 @@ for i in range(10):
     time.sleep(0.3)
 
 print()
+
+
+# ============================================================
+# INITIALIZE PH SENSOR
+# ============================================================
+
+try:
+
+    print("Initializing pH Sensor...")
+
+    ph_sensor = ADC(machine.Pin(PH_PIN))
+    ph_sensor.atten(ADC.ATTN_11DB)
+    ph_sensor.width(ADC.WIDTH_12BIT)
+
+    print("pH Sensor OK\n")
+
+except Exception as e:
+
+    print("pH Sensor Initialization Failed")
+    print(e)
+    raise SystemExit
+
+
+
 
 # ============================================================
 # CONNECT WIFI
@@ -244,6 +278,39 @@ while True:
         print("Dry Value :", DRY_VALUE)
         print("Wet Value :", WET_VALUE)
         print("Moisture  : {:.1f}%".format(soil_percent))
+
+        # ----------------------------------------------------
+        # pH Sensor
+        # ----------------------------------------------------
+
+        print("\nReading pH Sensor...")
+
+        ph_samples = []
+
+        for i in range(10):
+          ph_samples.append(ph_sensor.read())
+          time.sleep_ms(20)
+
+        ph_raw = sum(ph_samples) / len(ph_samples)
+
+        print("ADC Samples :", ph_samples)
+        print("ADC Average :", ph_raw)
+
+        # Convert ADC to voltage
+        voltage = (ph_raw / 4095.0) * 3.3
+
+        # Convert voltage to pH
+        ph_value = (PH_SLOPE * voltage) + PH_OFFSET
+
+        # Limit displayed values
+        if ph_value < 0:
+          ph_value = 0
+
+        if ph_value > 14:
+          ph_value = 14
+
+        print("Voltage  : {:.3f} V".format(voltage))
+        print("pH Value : {:.2f}".format(ph_value))
 
         # ----------------------------------------------------
         # Compare Previous Reading
